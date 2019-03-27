211b56b42cc3456b8885b55c4ba40dedb2366b2c
yankfy <yankfy@qq.com>
2019-03-27 14:35:10 +0800

    ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
-// import './home.dart';
+import './compontents/bottom_nav.dart';
+import 'package:amap_location/amap_location.dart';
 
-void main() => runApp(MyApp());
+void main() => {
+      AMapLocationClient.setApiKey('7c643109d6c50331e82d3dc15e400dea'),
+      runApp(new MyApp())
+    };
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
-      // theme: ThemeData.light(),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('List Page')),
-      body: Center(
-        child: Text('home页面'),
-      ),
-    );
+        title: 'Flutter bottomNavigationBar',
+        theme: ThemeData.light(),
+        home: BottomNavigationWidget());
   }
 }
---
+++ lib/pages/carema_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class CaremaScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text('CAREMA'),
+      ),
+      body:Center(
+        child: Text('CAREMA'),
+      )
+    );
+  }
+}
