43293abcc65a11a49aad4a521db4f5cfdd74afc4
wujinping0516 <wujinping0516@163.com>
2019-03-29 15:12:00 +0800

    ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,36 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+#import "PocRootViewController.h"
+#import <AMapFoundationKit/AMapFoundationKit.h>
+#define kAMapKit_APPKEY @"f4229f07c1407159db8ae14adb2282f3"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    
+    /**  高德地图 */
+//    [self initAMapKit];
+//    self.window.rootViewController = [self initialRootViewController];
+    
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (UIViewController *)initialRootViewController {
+
+    PocRootViewController *tabBarVC = [[PocRootViewController alloc] init];
+    return tabBarVC;
+}
+
+/** 初始化高德地图 APi */
+-(void)initAMapKit{
+    if ([kAMapKit_APPKEY length] == 0){
+        return;
+    }
+    [AMapServices sharedServices].apiKey = kAMapKit_APPKEY;
 }
 
 @end
---
+++ ios/Runner/Runner/Class/Home/Controller/FlutterContainerVC.h
@@ -0,0 +1,17 @@
+//
+//  FlutterContainerVC.h
+//  Runner
+//
+//  Created by Kipling on 2019/3/27.
+//  Copyright © 2019年 The Chromium Authors. All rights reserved.
+//  容器视图
+
+#import "ZCBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FlutterContainerVC : ZCBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
